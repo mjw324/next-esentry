8ddad79846b1ab885d7e45e1f48b28969547b15b
Micah Worth <mjw324@lehigh.edu>
2024-08-10 18:42:28 -0400

    chore: add newline at the end of next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -4,5 +4,6 @@ const nextConfig = {
     basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
     assetPrefix: process.env.URL ? process.env.URL : undefined,
   };
-  
-  export default nextConfig;
+
+
+export default nextConfig;
