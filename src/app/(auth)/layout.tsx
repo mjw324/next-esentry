7bd094a0b93499916e7dcb0bbb5350487b8920ce
Micah Worth <mjw324@lehigh.edu>
2024-08-11 21:42:53 -0400

    Added more padding to auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from 'react';
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="container mx-auto xl:px-0">
-      <div className="pt-8 mx:auto">
+      <div className="p-5 mx:auto">
         <Link href="/" className="group flex items-center gap-2 sm:gap-4">
           <ArrowChevronBack className="stroke-foreground group-hover:stroke-muted-foreground" />
           <span className="group-hover:text-muted-foreground">Back to Home</span>
